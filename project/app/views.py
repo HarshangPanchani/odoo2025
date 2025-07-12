f4963be48634c03d76cafe5d08db108da231c962
KISHAN PATEL <kishan7112@gmail.com>
2025-07-12 14:52:28 +0530

    MAINV1

--- project/app/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# from django.contrib.auth import get_user_model
-
-# User = get_user_model()
-
-
-def index(request):
-    return render(request, 'index.html')
